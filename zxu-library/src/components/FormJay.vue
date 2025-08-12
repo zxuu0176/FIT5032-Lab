7b507aca225cae442d5783a937b5362f1612aa06
zxuu0176 <zxuu0176@student.monash.edu>
2025-08-12 20:30:12 +1000

    fnished activity 1-6 including the clearform button

--- zxu-library/src/components/FormJay.vue
+++ zxu-library/src/components/FormJay.vue
@@ -18,7 +18,7 @@
             <div class="col-md-6">
               <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="isAustralian" v-model="formData.isAustralian">
-                <label class="form-check-label" for="isAustralian">Australian Resident? </label>
+                <label class="form-check-label" for="isAustralian">Australian Resident?</label>
               </div>
             </div>
             <div class="col-md-6">
@@ -42,12 +42,68 @@
       </div>
     </div>
   </div>
+
+  <div class="row mt-5" v-if="submittedCards.length">
+   <div class="d-flex flex-wrap justify-content-start">
+      <div v-for="(card, index) in submittedCards" :key="index" class="card m-2" style="width: 18rem;">
+         <div class="card-header">
+            User Information
+         </div>
+         <ul class="list-group list-group-flush">
+            <li class="list-group-item">Username: {{ card.username }}</li>
+            <li class="list-group-item">Password: {{ card.password }}</li>
+            <li class="list-group-item">Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}</li>
+            <li class="list-group-item">Gender: {{ card.gender }}</li>
+            <li class="list-group-item">Reason: {{ card.reason }}</li>
+         </ul>
+      </div>
+   </div>
+</div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
+const formData = ref({
+    username: '',
+    password: '',
+    isAustralian: false,
+    reason: '',
+    gender: ''
+});
+
+const submittedCards = ref([]);
+
+const submitForm = () => {
+    submittedCards.value.push({
+        ...formData.value
+    });
+};
+
+const clearForm = () => {
+    formData.value = {
+      username: '',
+      password: '',
+      isAustralian: false,
+      reason: '',
+      gender: ''
+    };
+};
 </script>
 
 <style scoped>
-
+   .card {
+   border: 1px solid #ccc;
+   border-radius: 10px;
+   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+   }
+   .card-header {
+   background-color: #275FDA;
+   color: white;
+   padding: 10px;
+   border-radius: 10px 10px 0 0;
+   }
+   .list-group-item {
+   padding: 10px;
+   }
 </style>
--- zxu-library/src/main.js
+++ zxu-library/src/main.js
@@ -1,9 +1,7 @@
-import './assets/main.css'
-// import './style.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
