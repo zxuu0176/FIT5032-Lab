7dec29aa6b255baf12c0c0766cbdf5f7170dc044
zxuu0176 <zxuu0176@student.monash.edu>
2025-08-12 19:10:04 +1000

    Updated week1 and week2 lab code to a general repository

---
+++ vue-project-week1/src/components/MyName.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="my-name">
+    <h1 class="green">Welcome, {{ name }}!</h1>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+.my-name h1 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .my-name h1 {
+    text-align: left;
+  }
+}
+</style>
